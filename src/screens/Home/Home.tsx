94d7bba1d3b677ab1c41f9e139cc07edeb25ce4d
Edinson Requena <edinsonrequena@protonmail.com>
2023-04-12 23:59:08 -0400

    feat: header home component

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View} from 'react-native';
 
 import {ButtonComponent} from '../../components/Button/Button';
+import {Header} from './Header';
 
 export const Home = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hola Mundo</Text>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Header user="Edinson Requena" />
       <ButtonComponent title="Hola" />
     </View>
   );
